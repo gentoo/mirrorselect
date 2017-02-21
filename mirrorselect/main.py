gentoo/mirrorselect

commit c91509d823c27fdb1527c61f77c959b305170915

main.py: Fix Bytes error in hosts list bug 610016
The code was only checking the first host which was not a bytes instance
in that case.  This resulted in the remaining host entries to not be decoded.

Signed-off-by: Brian Dolbec <dolsen@gentoo.org>

mirrorselect/main.py
@@ -108,8 +108,9 @@ class MirrorSelect(object):
 		else:
 			var = 'GENTOO_MIRRORS'
 
-		if hasattr(hosts[0], 'decode'):
-			hosts = [x.decode('utf-8') for x in hosts]
+		for i in range(0, len(hosts)):
+			if isinstance(hosts[i], 'bytes'):
+				hosts[i] = hosts[i].decode('utf-8')
 
 		if var == "sync-uri" and out:
 			mirror_string = '%s = %s' % (var, ' '.join(hosts))

commit 18df6efa96f57830f26ca94c41e1f1e8cc30b3ca

main.py: fix isinstance(hosts[i], bytes)
Fixes: c91509d823c2 ("main.py: Fix Bytes error in hosts list bug 610016")

mirrorselect/main.py
@@ -109,7 +109,7 @@ class MirrorSelect(object):
 			var = 'GENTOO_MIRRORS'
 
 		for i in range(0, len(hosts)):
-			if isinstance(hosts[i], 'bytes'):
+			if isinstance(hosts[i], bytes):
 				hosts[i] = hosts[i].decode('utf-8')
 
 		if var == "sync-uri" and out: