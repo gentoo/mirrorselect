gentoo/mirrorselect

commit c6c2635e9b78e6a14d614494d5b42432b89c36f0

selectors: fix exception name
Fixes: 1c6189be723e5048737f45cf531e35ec8725b727
Signed-off-by: Sam James <sam@gentoo.org>

mirrorselect/selectors.py
@@ -437,7 +437,7 @@ class Deep:
                 2,
             )
             return (None, True)
-        except IncompleteRead as e:
+        except httplib.IncompleteRead as e:
             self.output.write(
                 ("\ndeeptime(): download from host %s " "failed for ip %s: %s\n")
                 % (url_parts.hostname, ip, e),

commit 193979cfb87a16769b61e16c15e9ebf9db43bab6

selectors: fix exception name again
Fixes: c6c2635e9b78e6a14d614494d5b42432b89c36f0
Signed-off-by: Sam James <sam@gentoo.org>

mirrorselect/selectors.py
@@ -437,7 +437,7 @@ class Deep:
                 2,
             )
             return (None, True)
-        except httplib.IncompleteRead as e:
+        except http.client.IncompleteRead as e:
             self.output.write(
                 ("\ndeeptime(): download from host %s " "failed for ip %s: %s\n")
                 % (url_parts.hostname, ip, e),