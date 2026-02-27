gentoo/mirrorselect

commit 1c6189be723e5048737f45cf531e35ec8725b727

Drop Python 2 support
Signed-off-by: Sam James <sam@gentoo.org>

mirrorselect/selectors.py
@@ -37,26 +37,13 @@ import sys
 import time
 import hashlib
 
-if sys.version_info[0] >= 3:
-	import urllib.request, urllib.parse, urllib.error
-	url_parse = urllib.parse.urlparse
-	url_unparse = urllib.parse.urlunparse
-	url_open = urllib.request.urlopen
-	url_request = urllib.request.Request
-	HTTPError = urllib.error.HTTPError
-	import http.client
-	IncompleteRead = http.client.IncompleteRead
-else:
-	import urllib2
-	import urlparse
-	url_parse = urlparse.urlparse
-	url_unparse = urlparse.urlunparse
-	url_open = urllib2.urlopen
-	url_request = urllib2.Request
-	HTTPError = urllib2.HTTPError
-	import httplib
-	IncompleteRead = httplib.IncompleteRead
-
+import urllib.request, urllib.parse, urllib.error
+url_parse = urllib.parse.urlparse
+url_unparse = urllib.parse.urlunparse
+url_open = urllib.request.urlopen
+url_request = urllib.request.Request
+HTTPError = urllib.error.HTTPError
+import http.client
 
 from mirrorselect.output import encoder, get_encoding, decode_selection
 

mirrorselect/mirrorparser3.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """Mirrorselect 2.x
  Tool for selecting Gentoo source and rsync mirrors.
 
-Copyright 2009-2012 Gentoo Foundation
+Copyright 2009-2023 Gentoo Authors
 
 	Copyright (C) 2009 Sebastian Pipping <sebastian@pipping.org>
 	Copyright (C) 2009 Christian Ruppert <idl0r@gentoo.org>
@@ -81,14 +81,9 @@ class MirrorParser3:
 
 if __name__ == '__main__':
 	import sys
-	if sys.version_info[0] >= 3:
-		import urllib.request, urllib.parse, urllib.error
-		parser = MirrorParser3()
-		parser.parse(urllib.request.urlopen(MIRRORS_3_XML).read())
-	else:
-		import urllib
-		parser = MirrorParser3()
-		parser.parse(urllib.urlopen(MIRRORS_3_XML).read())
+	import urllib.request, urllib.parse, urllib.error
+	parser = MirrorParser3()
+	parser.parse(urllib.request.urlopen(MIRRORS_3_XML).read())
 	print('===== tuples')
 	print(parser.tuples())
 	print('===== uris')

commit c6c2635e9b78e6a14d614494d5b42432b89c36f0

selectors: fix exception name
Fixes: 1c6189be723e5048737f45cf531e35ec8725b727
Signed-off-by: Sam James <sam@gentoo.org>

mirrorselect/selectors.py
@@ -437,7 +437,7 @@ class Deep:
                 2,
             )
             return (None, True)
-        except IncompleteRead as e:
+        except httplib.IncompleteRead as e:
             self.output.write(
                 ("\ndeeptime(): download from host %s " "failed for ip %s: %s\n")
                 % (url_parts.hostname, ip, e),